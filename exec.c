89103254fb1afa015a859eec6243c5863adbca5b
Monday <hencynikky@gmail.com>
2023-11-10 05:00:05 -0800

    fix for exec

--- exec.c
+++ exec.c
@@ -2,22 +2,32 @@
 
 void execute(const char *commands)
 {
-	pid_t child_pid = fork();
+        pid_t child_pid = fork();
 
-	if (child_pid == -1){
-		perror("fork");
-		exit(EXIT_FAILURE);
-	} else if (child_pid == 0)
-	{
-		execlp(commands, commands, (char *)NULL);
-		perror("execlp");
-		exit(EXIT_FAILURE);
-	} else 
-	{
-		wait(NULL);
+        if (child_pid == -1){
+                perror("Error forking process");
+                exit(EXIT_FAILURE);
+        } else if (child_pid == 0)
+        {
+                char arg[256];
+                int count = 0;
 
-	
-	}
+                char *token = strtok((char *)command,"");
+                while (token != NULL)
+                {
+                arg[count++] = token;
+                }
+                arg[count] = NULL;
+
+                execvp(arg[0], arg);
+
+                h_print("Error Executing command. \n");
+                exit(EXIT_FAILURE);
+        } else
+        {
+                wait(NULL);
+
+
+        }
 
 }
-
